18b941f16e874ebce27004c6b731fb31289cd3b5
Alex Karamushko <alexander.karamushko@gmail.com>
2016-05-10 21:48:55 +0300

    changes

--- application/views/catalog/product.php
+++ application/views/catalog/product.php
@@ -38,13 +38,14 @@
 				<div class="col-center">
 					<header class="flx flx-justify">
 						<div class="price-holder"><span class="ammount"><?php echo $product->price; ?></span><span
-								<?php echo ($product->status!=0)?'class="avail"':'class="notavail"'?>><?php echo ($product->status!=0)?'Available':'Not Available'?></span></div>
+								<?php echo ( $product->status != 0 ) ? 'class="avail"' : 'class="notavail"' ?>><?php echo ( $product->status != 0 ) ? 'Available' : 'Not Available' ?></span>
+						</div>
 						<div class="action-holder">
 							<form action="<?php echo URL::base( true, false ); ?>cart/add" method="POST">
 								<input type="hidden" name="product_id" value="<?php echo $product->id; ?>">
 								<input type="hidden" name="product_qty" value="1">
 								<input type="submit" name="add_to_cart" value="Kosárba"
-								       class="btn bold solid green" <?php echo ($product->status==0)?'disabled':'';?>>
+								       class="btn bold solid green" <?php echo ( $product->status == 0 ) ? 'disabled' : ''; ?>>
 							</form>
 							<!--<div class="row flx-justify"><a id="hold-over" href="#" class="link blue lined">Hold over</a><a id="follow-price" href="#" class="link blue lined">Folow the price</a></div>-->
 						</div>
@@ -90,15 +91,17 @@
 						<div class="gray-wall size-checkbox-holder">
 							<h3>Méret szerint</h3>
 							<?php
-							foreach(json_decode($product->product_size) as $size){
-								if ( $size == 1 ) {
-									echo 'Icipici ';
-								} elseif ( $size == 2 ) {
-									echo 'Éppen jó ';
-								} elseif($size == 3) {
-									echo 'Óriás ';
-								} else {
-									echo 'Nem méretfüggő ';
+							if ( is_array( json_decode( $product->product_size ) ) ) {
+								foreach ( json_decode( $product->product_size ) as $size ) {
+									if ( $size == 1 ) {
+										echo 'Icipici ';
+									} elseif ( $size == 2 ) {
+										echo 'Éppen jó ';
+									} elseif ( $size == 3 ) {
+										echo 'Óriás ';
+									} else {
+										echo 'Nem méretfüggő ';
+									}
 								}
 							}
 							/**/
@@ -151,10 +154,10 @@
 			<div class="row tabs"></div>
 			<div id="tabs">
 				<ul>
-					<?php if($product->product_about<>''): ?>
+					<?php if ( $product->product_about <> '' ): ?>
 						<li><a href="#tabs-1">Leírás</a></li>
 					<?php endif; ?>
-					<?php if($product->product_composition<>''): ?>
+					<?php if ( $product->product_composition <> '' ): ?>
 						<li><a href="#tabs-2">Összetevők</a></li>
 					<?php endif; ?>
 					<?php if ( count( $aimages ) > 0 ): ?>
@@ -163,19 +166,19 @@
 					<?php if ( $product->video_link <> '' ): ?>
 						<li><a href="#tabs-4">Videó</a></li>
 					<?php endif; ?>
-					<?php if($current_user || count( $reviews )>0):?>
+					<?php if ( $current_user || count( $reviews ) > 0 ): ?>
 						<li><a href="#tabs-5">Értékelések (<?php echo count( $reviews ); ?>)</a></li>
-					<?php endif;?>
+					<?php endif; ?>
 				</ul>
-				<?php if($product->product_about<>''): ?>
-				<div id="tabs-1">
-					<?php echo $product->product_about; ?>
-				</div>
+				<?php if ( $product->product_about <> '' ): ?>
+					<div id="tabs-1">
+						<?php echo $product->product_about; ?>
+					</div>
 				<?php endif; ?>
-				<?php if($product->product_composition<>''): ?>
-				<div id="tabs-2">
-					<?php echo $product->product_composition; ?>
-				</div>
+				<?php if ( $product->product_composition <> '' ): ?>
+					<div id="tabs-2">
+						<?php echo $product->product_composition; ?>
+					</div>
 				<?php endif; ?>
 				<?php if ( count( $aimages ) > 0 ): ?>
 					<div id="tabs-3">
@@ -186,77 +189,79 @@
 					</div>
 				<?php endif; ?>
 				<?php if ( $product->video_link <> '' ): ?>
-						<?php
-					preg_match('#.*\/(.*)$#',$product->video_link,$matches);
+					<?php
+					preg_match( '#.*\/(.*)$#', $product->video_link, $matches );
 					?>
 					<div id="tabs-4">
 						<div class="video-box">
-							<iframe width="1140" height="640" src="https://www.youtube.com/embed/<?php echo $matches[1];?>" frameborder="0"
+							<iframe width="1140" height="640"
+							        src="https://www.youtube.com/embed/<?php echo $matches[1]; ?>" frameborder="0"
 							        allowfullscreen></iframe>
 						</div>
 					</div>
 				<?php endif; ?>
-				<?php if($current_user || count( $reviews )>0):?>
-				<div id="tabs-5">
-					<div class="row flx-column comment-box">
-						<div class="contact-form review-form">
-							<?php if ( $current_user ): ?>
-								<form action="" method="POST" class="row flx-justify" id="contact">
-									<div class="field-holder">
-										<input type="text" id="customer_firstname" name="customer_firstname"
-										       placeholder="<?php echo __( 'Keresztnév*' ); ?>"
-										       value="<?= ( $current_user ) ? $current_user->customer_firstname : '' ?>"
-										       required
-										       aria-required="true">
+				<?php if ( $current_user || count( $reviews ) > 0 ): ?>
+					<div id="tabs-5">
+						<div class="row flx-column comment-box">
+							<div class="contact-form review-form">
+								<?php if ( $current_user ): ?>
+									<form action="" method="POST" class="row flx-justify" id="contact">
+										<div class="field-holder">
+											<input type="text" id="customer_firstname" name="customer_firstname"
+											       placeholder="<?php echo __( 'Keresztnév*' ); ?>"
+											       value="<?= ( $current_user ) ? $current_user->customer_firstname : '' ?>"
+											       required
+											       aria-required="true">
 									<textarea id="product_review" name="product_review"
 									          placeholder=""
 									          required aria-required="true"></textarea>
-										<select name="rating">
-											<option value="1">1</option>
-											<option value="2">2</option>
-											<option value="3">3</option>
-											<option value="4">4</option>
-											<option value="5">5</option>
-										</select>
-										<input type="hidden" name="product_id" value="<?php echo $product->id ?>">
-										<input id="submit" type="submit" value="<?php echo __( 'ELKÜLDÖM' ); ?>"
-										       class="btn solid pink">
-									</div>
+											<select name="rating">
+												<option value="1">1</option>
+												<option value="2">2</option>
+												<option value="3">3</option>
+												<option value="4">4</option>
+												<option value="5">5</option>
+											</select>
+											<input type="hidden" name="product_id" value="<?php echo $product->id ?>">
+											<input id="submit" type="submit" value="<?php echo __( 'ELKÜLDÖM' ); ?>"
+											       class="btn solid pink">
+										</div>
 
-								</form>
-							<?php endif; ?>
-						</div>
-						<?php if ( count( $reviews ) > 0 ): ?>
-							<?php foreach ( $reviews as $item ): ?>
-								<div id="comment-1" class="row comment-item">
-									<div class="container">
-										<div class="meta"><span class="author"><?php echo $item->firstname; ?></span>
+									</form>
+								<?php endif; ?>
+							</div>
+							<?php if ( count( $reviews ) > 0 ): ?>
+								<?php foreach ( $reviews as $item ): ?>
+									<div id="comment-1" class="row comment-item">
+										<div class="container">
+											<div class="meta"><span
+													class="author"><?php echo $item->firstname; ?></span>
 
-											<div class="row rate-holder">
-												<?php
-												for ( $i = 0; $i < 5; $i ++ ) {
-													if ( $i < $item->rating ) {
-														echo '<span class="star solid"></span>';
-													} else {
-														echo '<span class="star"></span>';
+												<div class="row rate-holder">
+													<?php
+													for ( $i = 0; $i < 5; $i ++ ) {
+														if ( $i < $item->rating ) {
+															echo '<span class="star solid"></span>';
+														} else {
+															echo '<span class="star"></span>';
+														}
 													}
-												}
-												?>
+													?>
+												</div>
 											</div>
 										</div>
-									</div>
-									<div class="container">
-										<div class="content">
-											<p><?php echo $item->review; ?></p>
+										<div class="container">
+											<div class="content">
+												<p><?php echo $item->review; ?></p>
+											</div>
+											<br><span rel="date"
+											          class="date"><?php echo $item->date ?></span>
 										</div>
-										<br><span rel="date"
-										          class="date"><?php echo $item->date ?></span>
 									</div>
-								</div>
-							<?php endforeach; ?>
-						<?php endif; ?>
+								<?php endforeach; ?>
+							<?php endif; ?>
+						</div>
 					</div>
-				</div>
 				<?php endif; ?>
 			</div>
 			<!-- end .row.tabs-->
@@ -275,27 +280,30 @@
 			<div id="carusel-2" class="carusel-container">
 				<ul id="carusel-line">
 					<?php
-					$products = ORM::factory( 'Products' )->where('status','=',1)->order_by( 'sales', 'DESC' )->limit( 6 )->find_all();
+					$products = ORM::factory( 'Products' )->where( 'status', '=', 1 )->order_by( 'sales', 'DESC' )->limit( 6 )->find_all();
 					foreach ( $products as $product ):
 						$image = ORM::factory( 'ProductImages' )->where( 'product_id', '=', $product->id )->limit( 1 )->find();
 						?>
-						<li class="item product-preview <?php echo ($product->on_sale==1)?'label-sale ':''; echo ($product->new==1)?'label-new ':''; ?>">
-							<a class="preview-link" href="<?php echo URL::base( true, false ); ?>catalog/product/<?php echo $product->id ?>">
-							<?php if ( $image->img_name != '' ): ?>
-								<img
-									src="<?= URL::base( true, false ) ?>uploads/products/<?php echo $image->img_name; ?>">
-							<?php else: ?>
-								<img src="<?= URL::base( true, false ) ?>assets/img/placeholder.png">
-							<?php endif; ?>
-								</a>
+						<li class="item product-preview <?php echo ( $product->on_sale == 1 ) ? 'label-sale ' : '';
+						echo ( $product->new == 1 ) ? 'label-new ' : ''; ?>">
+							<a class="preview-link"
+							   href="<?php echo URL::base( true, false ); ?>catalog/product/<?php echo $product->id ?>">
+								<?php if ( $image->img_name != '' ): ?>
+									<img
+										src="<?= URL::base( true, false ) ?>uploads/products/<?php echo $image->img_name; ?>">
+								<?php else: ?>
+									<img src="<?= URL::base( true, false ) ?>assets/img/placeholder.png">
+								<?php endif; ?>
+							</a>
+
 							<div class="row flx-justify">
 								<h2><?php echo $product->product_name; ?></h2>
-								<?php if ($product->on_sale==1): ?>
+								<?php if ( $product->on_sale == 1 ): ?>
 									<span class="ammount old"><?php echo $product->price; ?></span>
 									<span class="ammount"><?php echo $product->sale_price; ?></span>
 								<?php else: ?>
 									<span class="ammount"><?php echo $product->price; ?></span>
-								<?php endif;?>
+								<?php endif; ?>
 								<form action="<?php echo URL::base( true, false ); ?>cart/add" method="POST">
 									<input type="hidden" name="product_id" value="<?php echo $product->id; ?>">
 									<input type="hidden" name="product_qty" value="1">
