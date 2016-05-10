984ecdf185ea93ed63fc74b125106aebe5aefb8e
Alex Karamushko <alexander.karamushko@gmail.com>
2016-05-10 13:39:30 +0300

    user profile

--- application/views/order/step3.php
+++ application/views/order/step3.php
@@ -440,7 +440,7 @@ if ( isset( $session['step2'] ) ) {
 					</div>
 					<div class="row flx-center check-box-holder">
 						<input type="checkbox" id="confirm">
-						<label for="confirm" class="gray-box__green-check">Elolvastam és elfogadom az <a href="/aszf.pdf">Általános Szerződési Feltételekben leírtakat!</a></label>
+						<label for="confirm" class="gray-box__green-check">Elolvastam és elfogadom az <a style="text-decoration: underline" href="/aszf.pdf">Általános Szerződési Feltételekben leírtakat!</a></label>
 					</div>
 					<div class="row flx-center action-holder">
 						<input type="submit" name="submit" id="submit" value="<?php echo __('MEGRENDELEM'); ?>" class="btn large solid green">
