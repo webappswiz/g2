32c160049737c809b8aec26873c5c2ffcc499a58
Alex Karamushko <alexander.karamushko@gmail.com>
2016-05-12 21:36:18 +0300

    fixes

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -539,6 +539,16 @@ $(function () {
             );
     });
 
+    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+    if (d.getElementById(id)) {
+        return;
+    }
+    js = d.createElement('script');
+    js.id = id;
+    js.async = true;
+    js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
+    ref.parentNode.insertBefore(js, ref);
+
     var _fbq = window._fbq || (window._fbq = []);
     if (!_fbq.loaded) {
         var fbds = document.createElement('script');
@@ -587,16 +597,8 @@ $(function () {
         FB.ui(obj, callback);
     }
 
-});
 
-(function (d, debug) {
-    var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement('script');
-    js.id = id;
-    js.async = true;
-    js.src = "//connect.facebook.net/en_US/all" + (debug ? "/debug" : "") + ".js";
-    ref.parentNode.insertBefore(js, ref);
-}(document, /*debug*/ false));
+
+
+
+});
